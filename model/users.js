1af8f32f51ea0a826749f2e22b739a73a3738dac
Chen Fan <vincebox9111@gmail.com>
2018-02-25 19:59:46 -0500

    db connection updated;

--- model/users.js
+++ model/users.js
@@ -1,26 +1,24 @@
-const connection = require('./connection').connection;
+const pool = require('./connection').pool;
 
 const getUserById = function(id) {
-    let sql = `SELECT * from users WHERE id = ${id};`;
-    connection.connect();
-
     return new Promise((resolve, reject) => {
+        let sql = `SELECT * from users WHERE id = ${id};`;
+
         if (!id) {
             let err = new Error('Bad Request');
             err.status = 400;
             reject(err);
         }
 
-        connection.query(sql, function(err, rows, fields) {
+        pool.query(sql, function(err, rows, fields) {
             if (err) {
                 let err = new Error('Network Error');
                 err.status = 500;
                 reject(err);
             }
-            console.log(rows);
+            // console.log(rows);
             resolve(rows[0]);
         });
-        connection.end();
     });
 };
 
